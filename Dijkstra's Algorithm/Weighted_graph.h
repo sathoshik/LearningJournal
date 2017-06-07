48d89b9cd341c07280240138d8794e598ed01a27
Sathoshi Kumarawadu <sathoshi.k@gmail.com>
2017-06-06 23:12:04 -0400

    code review: refactoring

--- Dijkstra's Algorithm/Weighted_graph.h
+++ Dijkstra's Algorithm/Weighted_graph.h
@@ -1,7 +1,7 @@
 /*****************************************
- * UW User ID:  uwuserid
+ * UW User ID:  spkumara
  * Submitted for ECE 250
- * Semester of Submission:  (Winter|Spring|Fall) 20NN
+ * Semester of Submission:  Winter 2017
  *
  * By submitting this file, I affirm that
  * I am the author of all modifications to
@@ -37,7 +37,6 @@ struct vertex {
       degree(0),
       heapIndex(0),
       tailIndex(-1){
-      //nothing happens in the constructor
       }
 
 };
@@ -49,7 +48,6 @@ struct adjacentNode {
   adjacentNode():
       weight(0),
       next(nullptr){
-      //nothing happens in the construct
       }
 
 };
@@ -81,11 +79,9 @@ class Heap {
     int extractMin();
     void modifyKey(int);
 
-    //TODO
     void print();
 
 };
-
 
 Heap::Heap(int m, int s, vertex *graph):
   size(1), //always create a heap of size 1 initially
@@ -183,17 +179,9 @@ void Heap::heapify( int i ) {
       break;
     }
   }
-  
-
-/*
-  //cout << "finished heapifying at " << i << ". swapped " << i <<" with " << smallestIndex << ". heap size: " << size << endl;
-  //finished heapifying
-  return; */
-
 }
 
 int Heap::extractMin() {
-  //print(); 
   if (isEmpty()){
     throw underflow();
   }
@@ -208,44 +196,24 @@ int Heap::extractMin() {
   //cout << "Extract min value of index of " << min << endl;
   //call heapify on the first index.
   heapify(1);
-  //print();
-  //cout << "After reorganizing heap from extracting min " << endl;
-  //print();
-
+    
   return min;
 }
 
 void Heap::modifyKey(int i) {
 
- //cout << "parent of " << i << " is " << parent(i) << endl;
- //cout << "left of "<< i <<" is " << left(i) << endl;
- //cout << "right of " << i << " is " << right(i) << endl;
+  if ( vertices[array[parent(i)]].depth >= vertices[array[i]].depth) {
+    while ( i > 1 && vertices[array[parent(i)]].depth > vertices[array[i]].depth ) {
 
- if ( vertices[array[parent(i)]].depth >= vertices[array[i]].depth) {
-  //cout << " Reference depth is >= depth of node currenlty comparing " << endl;
-  //cout << "Upward fix " << endl;
-  //print();
-  while ( i > 1 && vertices[array[parent(i)]].depth > vertices[array[i]].depth ) {
-    
-
-    int temp = parent(i);
-    std::swap(array[i],array[temp]);
-    vertices[array[i]].heapIndex = i;
-    vertices[array[temp]].heapIndex = temp;
-    i = temp;
-  }
-
-  //cout << "End of upward fix " << endl;
-  //print();
-
- } else {
+      int temp = parent(i);
+      std::swap(array[i],array[temp]);
+      vertices[array[i]].heapIndex = i;
+      vertices[array[temp]].heapIndex = temp;
+      i = temp;
+    }
+  } else {
   
-   //cout << "Downward fix " << endl;
-   //int start_s = clock(); 
    heapify(i);
-   //int stop_s = clock();
-   //cout << "dfix: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << endl;
-
  
  }
 }
@@ -319,9 +287,7 @@ class Weighted_graph {
     void relax(vertex, adjacentNode *, int) const;
     void print() const; // TODO
 
-    //TODO test method for shortestDistance
     double shortestPath(int, int) ;
-	// Friends
 
 	friend std::ostream &operator<<( std::ostream &, Weighted_graph const & );
 };
@@ -383,10 +349,7 @@ double Weighted_graph::adjacent(int m, int n) const {
 
 double Weighted_graph::shortestPath(int m, int n) {
     
-  //initialize New Heap.
   if ( previousIndex == -1 || previousIndex != m) {
-    //cout << "new starting " << endl;
-    //if the previous index that you searched is different, create a new heap.
     for (int i = 0; i < verticesCount; i++) {
       //Initialize the depth all vertices to INF 
       vertices[i].depth = INF; 
@@ -400,7 +363,6 @@ double Weighted_graph::shortestPath(int m, int n) {
     delete pq;
     pq = new Heap(verticesCount,m, vertices);
   } else if ( vertices[n].heapIndex == -1 ) {
-    //cout << "already exists " << endl;
     return vertices[n].depth;
   }
   while (!(pq -> isEmpty())) {
@@ -408,44 +370,28 @@ double Weighted_graph::shortestPath(int m, int n) {
     int peakCurrentNodeIndex = pq -> top();
 
     if (peakCurrentNodeIndex == n) {
-      //delete pq; //cleanup heap that was created.
-      //cout <<"about to return " << endl;
-      //insert node back in to maintain the heap.
       return vertices[peakCurrentNodeIndex].depth;
     }
 
     int currentNodeIndex = pq -> extractMin();
-
     adjacentNode *visitingNode = vertices[currentNodeIndex].adjacentList;
 
     while (visitingNode != nullptr ){
-     // cout << "Visiting new adjacent node" << endl;
        
       if (vertices[visitingNode - matrix[currentNodeIndex]].heapIndex == -1) {
-          //cout <<  "We have finished visiting this node. negative " << endl;
-          //If the heapIndex of the visiting Node is < 0, that means we have extracted it from the heap. Thus skip to next node
-          visitingNode = visitingNode -> next;
+        visitingNode = visitingNode -> next;
           continue;
         }
 
       if (vertices[visitingNode - matrix[currentNodeIndex]].heapIndex == -5 ) {
         //have not yet been discovered.
         //insert into the que
-        //cout << "This node has not been discovered yet. node : " << visitingNode -> reference - vertices << endl;
         relax(vertices[currentNodeIndex], visitingNode, currentNodeIndex);
         pq -> insert(visitingNode - matrix[currentNodeIndex]);
 
-        //cout << "Finished Inserting node to que. " << endl;
       } else {
-        //cout << " there are more adjacent nodes " << endl;
-        //cout << "Currently visiting node " << visitingNode -> reference - vertices;
-        //cout << " with weight " << visitingNode -> weight << endl;
-        //cout << "about to relax" << endl;
         relax(vertices[currentNodeIndex], visitingNode, currentNodeIndex);
-        //cout << "Finished relaxing. Weight have been updated " << endl;
         pq -> modifyKey(vertices[visitingNode - matrix[currentNodeIndex]].heapIndex);
-        //cout << "set next node" << endl;
-
       }
         visitingNode = visitingNode -> next;
     }
@@ -468,7 +414,6 @@ double Weighted_graph::distance( int m, int n){
     return INF;
   }
 
-  //implement distance algorithm.
   return shortestPath(m,n);
  
 }
@@ -488,25 +433,21 @@ void Weighted_graph::insert(int m, int n, double w) {
   }
   //check if the edge exists already.
    if (matrix[m][n].weight > 0 ) {
-    //edge between m and n exists
-   //cout << "edge between " << m << " and " << n << " already exists" << endl;
+   //edge between m and n exists
    //If edge already exists, its same to assume that its already in the linked list.
    //simply update the weight between the two edges.
    matrix[m][n].weight = w;
    matrix[n][m].weight = w;
    
-   //cout << "updated weights between " << m << " and " << n << endl;
   } else {
   
     if ( vertices[m].degree <=0 ) {
-      //cout << "vertex " << m << " has no edges yet. Create new adjacency list" << endl;
       //m x n relationship
       vertices[m].adjacentList = &matrix[m][n];
       vertices[m].degree ++;
       vertices[m].tailIndex = n;
       matrix[m][n].weight = w; //add weight between two edges
     } else {
-      //cout << "vertex " << m << " has adjacencies. Insert edge into list" << endl;
        // m x n relationship
       matrix[m][vertices[m].tailIndex].next = &matrix[m][n]; //add new edge to linked list
       vertices[m].degree ++;
@@ -515,15 +456,12 @@ void Weighted_graph::insert(int m, int n, double w) {
     }
 
     if (vertices[n].degree <= 0 ) {
-      //cout << "vertex " << n << " has no edges yet. Create new adjacency list" << endl;
       //n x m relationship
       vertices[n].adjacentList = &matrix[n][m];
       vertices[n].degree ++;
       vertices[n].tailIndex = m;
       matrix[n][m].weight = w;
     } else {
-      //cout << "vertex " << n << " has adjacencies. Insert edge into list" << endl;
-      //n x m relationshop
       //cout << vertices[n].tailIndex << endl;
       matrix[n][vertices[n].tailIndex].next = &matrix[n][m]; //add new edge to linked list
       vertices[n].degree ++;
